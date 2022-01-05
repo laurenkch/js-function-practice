aa8d940be332abe207210c96a49cf1c4a3b46bde
laurenkch <lkoch879@gmail.com>
2022-01-05 10:08:50 -0500

    with notes

--- scripts/exercises.js
+++ scripts/exercises.js
@@ -55,6 +55,14 @@
         }
     }
 
+    //could have used:
+  function isVowel(char) {
+      return 'aeiou'.includes(char);
+  }
+//because .includes is a boolean function and it returns true/false.
+  const isVowel = (char) => 'aeiou'.includes(char);
+
+
     // ---------------------
     // Write a function rovarspraket() that will translate a text into "rövarspråket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
     // ---------------------
@@ -77,6 +85,34 @@
         return updatedSentence;
     }
 
+    //could have:
+
+    function rovarspraket(text) {
+        const textArr = textArr.toLowerCase().split('');
+        for (let i = 0; i < textArr.length; i++) {
+            if('bcdfghjklmnprstovwxyz'.includes(textArr[i])) {
+                textArr[i] = textArr[i] + 'o' + textArr[i];
+            }
+        }
+        return textArr.join('');
+    }
+//or (this still changes it to an array, it just calls it 'text' the whole time.)
+    function rovarspraket(text) {
+        text = text.toLowerCase().split('');
+        for (let i = 0; i < textArr.length; i++) {
+            if('bcdfghjklmnprstovwxyz'.includes(text[i])) {
+                text[i] = text[i] + 'o' + text[i];
+            }
+        }
+        return textArr.join('');
+    }
+//or, this version calls a function against each part of the array we make.
+let translation = '';
+text.toLowerCase().split('').forEach(char => {
+    "bcdfghjklmnpqrstvwxyz".includes(char) ? translation += `${char}o${char}` : translation += char;
+})
+
+// testing against consonants is better than vowels because there are a large number of symbols we may have to account for, like $ or %.
 
     // ---------------------
     // Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
@@ -102,6 +138,11 @@
         return product;
     }
 
+//could have:
+
+const sum = arr => arr.reduce((acc,i) => acc +i);
+
+
     // ---------------------
     // Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
     // ---------------------
@@ -117,6 +158,19 @@
         }
         return backwardSentence;
     }
+
+//didn't need "charAt" could have just said "forwardSentence[i];"
+
+//could have also: (the order of the addition on the newString line makes it reversed.)
+function reverse (str) {
+    let newString = "";
+    for (let i=0; i < str.length; i++) {
+        newString = str [i] + newString;
+    }
+    return newString;
+}
+
+const reverse = string => string.split('').reverse().join('')
 
     // ---------------------
     // Define a function findLongestWord() that takes an array of words and returns the length of the longest one.
@@ -135,6 +189,18 @@
         return numberOfLetters;
 
     }
+  //  could have:
+    function findLongestWord(arr) {
+        let length = 0;
+        for(let i = 0; i < arr.length; i++) {
+            if(arr[i].length > length) {
+                length = arr[i].length;
+            }
+        }
+        return length;
+    }
+//need to look into changing variables within for loops more. 
+//set variable to 0 so you always get a value back, instead of undefined for an empty array.
 
     // ---------------------
     // Define a function filterLongWords() that takes an array of words and an integer i and returns the array of words that are longer than i.
@@ -152,6 +218,8 @@
             }
         return shortenedArray;
         }
+//could have also:
+const filterLongWords = (arr,i) => arr.filter(word => word.length >i);
 
     // ---------------------
     // Define a function charFreq() that takes a string and builds a frequency listing of the characters contained in it. Represent the frequency listing as a Javascript object. Try it with something like charFreq("abbabcbdbabdbdbabababcbcbab").
@@ -174,6 +242,29 @@
         }
     }
         return totals;
+
+//could have also:
+//because if you call a property that doesn't exist you get undefined = false.
+    function charFreq(str) {
+        const freqList = {};
+        for (let i = 0; i < str.length; i++ ) {
+            if(freqList(str[i])) { 
+                //if str[i] exists
+                freqList[str[i]] = freqList[str[i]] +1;
+            } else {
+                //if that property is undefined.
+                freqList[str[i]] = 1;
+            }
+        }
+    }
+}
+//could have also:
+
+function charFre(str) {
+    const freqList = {};
+    str.split (''.forEach(char => freqList[char] = freqList[char] + 1 || 1);
+    return freqList;
+    
 }
     ////////////////////////////////////////////////////////////////////////
     /////////////////////////DO NOT CHANGE CODE BELOW///////////////////////
